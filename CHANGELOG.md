8369ebd5be479205b1e499710652a05843375619
rans-rans <ransfordowusuansah9@gmail.com>
2024-03-03 21:10:34 +0000

    moved homepage to main.dart

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.2
+* Updated example main.dart file
+
 ## 0.0.1
 
 * Initial stable release, created by Ransford.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:launcher_switch/launcher_switch.dart';
 
-import 'home_screen.dart';
-
 final launcherSwitch = LauncherSwitch();
 
 void main() {
@@ -16,6 +14,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Laucher Switch"),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                launcherSwitch.switchIcon(
+                  iconKey: ".Icon2",
+                  previousIconKey: ".MainActivity",
+                );
+              },
+              child: const Text("Switch icon"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
